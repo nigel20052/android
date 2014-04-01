cfed180fd2485d1901bcbe1800952e0e799dac46
nigel20052 <nigel20052@gmail.com>
2014-03-31 20:57:41 -0600

    ajustes android

--- src/com/example/retrofit/MainActivity.java
+++ src/com/example/retrofit/MainActivity.java
@@ -7,6 +7,7 @@ import com.example.retrofit.APIConnector;
 import com.example.retrofit.MainActivity;
 import com.example.retrofit.R;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
@@ -21,9 +22,10 @@ public class MainActivity extends Activity {
 	
 	private EditText campo_usuario, campo_password;
 	private Button iniciar_sesion;
-	private String result;
 	private StorageDAO db;
 	JSONObject obj = null;
+	private String username;
+	private String password;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -40,27 +42,7 @@ public class MainActivity extends Activity {
 			public void onClick(View v) {
 				String username=campo_usuario.getText().toString();
 		        String password=campo_password.getText().toString();
-		        
-		        try {
-					login(username,password);
-				} catch (Exception e1) {
-					e1.printStackTrace();
-				}
-		        if(result != "false"){
-		        try {
-					updateDataMechanics(username,password);
-					updateDataTools(username,password);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-		       
-				Intent i = new Intent(MainActivity.this,SecondActivity.class);
-				startActivity(i);
-		        }
-		        else{
-		        	 Toast error = Toast.makeText(getApplicationContext(),"Usuario y/o Password incorrecto", Toast.LENGTH_LONG);
-		        	 error.show();
-		        }
+		        new APIConnectorLoginTask().execute(username,password);
 			}
 		});
 	}
@@ -70,44 +52,92 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+
+	public void updateDataMechanics(String username, String password) {
+			String mechanics;
+			try {
+				mechanics = APIConnector.getMechanics(username,password);
+				JSONArray jsonArray = new JSONArray(mechanics);
+				db.open();
+				db.deleteOldMechanics();
+				for (int i=0;i<jsonArray.length();i++){ 
+					obj = new JSONObject(jsonArray.get(i).toString());
+					db.insertMechanic(Integer.parseInt(obj.get("id").toString()),obj.get("code").toString(), obj.get("name").toString(),Integer.parseInt(obj.get("status").toString()));
+        		}	
+				db.close();
+			} 
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+	}
 	
-	public void login(String username,String password) throws Exception{
+	public void updateDataTools(String username, String password){
+			try {
+				String tools = APIConnector.getTools(username,password);
+				JSONArray jsonArray = new JSONArray(tools);
+        
+				db.open();
+				db.deleteOldTools();
+    	
+				for (int i=0;i<jsonArray.length();i++){ 
+					obj = new JSONObject(jsonArray.get(i).toString());
+					db.insertTool(Integer.parseInt(obj.get("id").toString()),obj.get("code").toString(), obj.get("name").toString());
+				}	
 		
-		String resultado = APIConnector.auth(username,password);
-		JSONObject jObj = null;
-		jObj = new JSONObject(resultado);
-		result = jObj.getString("success");
+				db.close();
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
 	}
 
-	public void updateDataMechanics(String username, String password) throws Exception{
+	private class APIConnectorLoginTask extends AsyncTask<String,String,String> {
+	
+		@Override
+		protected String doInBackground(String... params) {
+			String result;
+			try {
+				username = params[0];
+				password = params[1];
+				String resultado = APIConnector.auth(params[0], params[1]);
+				JSONObject jObj = null;
+				jObj = new JSONObject(resultado);
+				result = jObj.getString("success");
+			} catch (Exception e) {
+				e.printStackTrace();
+				result = "false";
+			}
+			return result;
+		}
+	
+		protected void onPostExecute(String result) {
+			if(result.equals("true")){
+				new APIConnectorCatalogsTask().execute("mechanics");
+				new APIConnectorCatalogsTask().execute("tools");
+        		Intent i = new Intent(MainActivity.this,SecondActivity.class);
+        		startActivity(i);
+			} else {
+				Toast error = Toast.makeText(getApplicationContext(),"Usuario y/o Password incorrecto", Toast.LENGTH_LONG);
+            	error.show();
+			}
+    	}//end post
+
+	}//end inner class
+	
+	private class APIConnectorCatalogsTask extends AsyncTask<String,String,Void> {
 		
-		String mechanics = APIConnector.getMechanics(username,password);
-		JSONArray jsonArray = new JSONArray(mechanics);
-        
-		db.open();
-    	db.deleteOldMechanics();
-    	
-		for (int i=0;i<jsonArray.length();i++){ 
-         obj = new JSONObject(jsonArray.get(i).toString());
-         db.insertMechanic(Integer.parseInt(obj.get("id").toString()),obj.get("code").toString(), obj.get("name").toString(),Integer.parseInt(obj.get("status").toString()));
-        }	
-		
-        db.close();
+		@Override
+		protected Void doInBackground(String... params) {
+				if ("mechanics".equals(params[0])){
+					updateDataMechanics(username,password);
+				}
+				if ("tools".equals(params[0])){
+					updateDataTools(username,password);
+				}
+			return null;
+		}
 	}
 	
-public void updateDataTools(String username, String password) throws Exception{
-		
-		String tools = APIConnector.getTools(username,password);
-		JSONArray jsonArray = new JSONArray(tools);
-        
-		db.open();
-    	db.deleteOldTools();
-    	
-		for (int i=0;i<jsonArray.length();i++){ 
-         obj = new JSONObject(jsonArray.get(i).toString());
-         db.insertTool(Integer.parseInt(obj.get("id").toString()),obj.get("code").toString(), obj.get("name").toString());
-        }	
-		
-        db.close();
-	}
+	
+
 }
